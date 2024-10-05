b24ae549329bff9ed784626a747ab6a841f0b85b
InvestUz <invest.abdusattorov@gmail.com>
2024-10-05 16:12:35 +0500

    fixed

--- resources/views/files/index.blade.php
+++ resources/views/files/index.blade.php
@@ -5,28 +5,28 @@
     <div class="row">
         <div class="col-12">
             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                <h4 class="mb-sm-0 font-size-18">Control Files</h4>
+                <h4 class="mb-sm-0 font-size-18">Файлы</h4>
 
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
                         <li class="breadcrumb-item">
                             <a href="{{ route('home') }}" class="text-primary">@lang('global.home')</a>
                         </li>
-                        <li class="breadcrumb-item active">Control Files</li>
+                        <li class="breadcrumb-item active">Файлы</li>
                     </ol>
                 </div>
             </div>
         </div>
     </div>
 
-    <!-- Uploaded Files Heading & Upload Button -->
+    <!-- Загруженные файлы Heading & Upload Button -->
     <div class="row align-items-center mb-4">
         <div class="col-md-6">
-            <h1 class="display-6 text-primary">Uploaded Files</h1>
+            <h1 class="display-6 text-primary">Загруженные файлы</h1>
         </div>
         <div class="col-md-6 text-end">
             <a class="btn btn-lg btn-outline-primary" href="{{ route('files.create') }}">
-                <i class="fas fa-upload"></i> Upload Files
+                <i class="fas fa-upload"></i> Загрузить файлы
             </a>
         </div>
     </div>
@@ -36,11 +36,16 @@
         <table class="table table-hover align-middle table-bordered">
             <thead class="table-light">
                 <tr>
-                    <th>Name</th>
-                    <th>Department</th>
-                    <th>File</th>
-                    <th>Created At</th>
-                    <th>Actions</th>
+                
+                        <th>Имя</th>
+                        <th>Отдел</th>
+                        <th>Файл</th>
+                        <th>Создано</th>
+                        <th>Действия</th>
+                    
+                    
+                    <th>Сайт (Trello)</th>
+
                 </tr>
             </thead>
             <tbody>
@@ -55,10 +60,16 @@
                         </td>
                         <td>{{ $file->created_at->format('d-m-Y H:i') }}</td>
                         <td>
-                            <button class="btn btn-sm btn-secondary" 
+                            <button class="btn btn-sm btn-secondary"
                                 onclick="copyToClipboard('{{ route('files.show', $file->slug) }}')">
                                 <i class="fas fa-copy"></i> Copy Link
                             </button>
+                        </td>
+
+                        <td>
+                            <a target="_blank" class="btn btn-sm btn-light" href="https://trello.com/b/r5YPolha/">Trello
+                                <img width="30" src="https://cdn.icon-icons.com/icons2/3053/PNG/512/trello_macos_bigsur_icon_189616.png" alt="">
+                            </a>
                         </td>
                     </tr>
                 @endforeach
