0b2877a36cf00ad456c5ffb5d81bdbd030fd4adc
InvestUz <invest.abdusattorov@gmail.com>
2024-10-09 15:04:24 +0500

    removing unneedable columns

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             SystemInitSeeder::class,
-            DepartmentSeeder::class,
-            PositionSeeder::class,
+       
             
         ]);
     }
